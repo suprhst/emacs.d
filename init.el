20e05ef9f751e981a730df4ba3279fe887bd8e79
suprhst <79753461+suprhst@users.noreply.github.com>
2022-01-09 20:47:12 +0530

    Modified Init

--- init.el
+++ init.el
@@ -15,25 +15,3 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/release.org"))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(ansi-color-names-vector
-   ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes '(underwater))
- '(custom-safe-themes
-   '("3263bd17a7299449e6ffe118f0a14b92373763c4ccb140f4a30c182a85516d7f" "db7f422324a763cfdea47abf0f931461d1493f2ecf8b42be87bbbbbabf287bfe" default))
- '(org-directory "~/org")
- '(package-selected-packages
-   '(multiple-cursors highlight-indent-guides rainbow-delimiters go-mode lsp-mode smartparens keycast auto-package-update gcmh expand-region org-appear htmlize ox-reveal org-roam-ui simple-httpd websocket org-roam org-superstar flycheck company treemacs-projectile projectile dashboard ivy-posframe try which-key hide-mode-line moody minions underwater-theme exotica-theme ivy-prescient all-the-icons-ivy-rich use-package treemacs-icons-dired treemacs-all-the-icons mode-icons ivy-rich counsel all-the-icons-ivy all-the-icons-dired)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(all-the-icons-dired-dir-face ((t (:foreground nil))))
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
